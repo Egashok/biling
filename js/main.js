b9ecc0eeac0709c94a4d039f325a3021b89b1366
egashok <55044715+Egashok@users.noreply.github.com>
2022-11-24 23:14:38 +0600

    jhig

--- js/main.js
+++ js/main.js
@@ -27,7 +27,24 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById("form",);
 
-})
+$(document).ready(function () {
+
+    //E-mail Ajax Send
+    $("form").submit(function () { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "../mail.php", //Change
+            data: th.serialize()
+        }).done(function () {
+            alert("Thank you!");
+            setTimeout(function () {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+});
